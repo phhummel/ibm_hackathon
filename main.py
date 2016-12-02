9d2a9871248351c8462c2198c8201a1d081ba572
Jan Hendrik Kirchner <janhendrikkirchner@83add7ab.funky.uni-osnabrueck.de>
2016-12-02 14:31:40 +0100

    added text-to-speech for answer of conversation

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import json
+from os.path import join, dirname
 from watson_developer_cloud import ConversationV1
+from watson_developer_cloud import TextToSpeechV1
+import uuid
+
 
 conversation = ConversationV1(
   username='d9940f19-3ca0-4154-88dd-af102b9b701f',
@@ -7,10 +11,17 @@ conversation = ConversationV1(
   version='2016-09-20'
 )
 
+text_to_speech = TextToSpeechV1(
+    username='28a1b1ed-4207-4397-96f8-13970d0778dd',
+    password='O8aPGSZ0ALGZ',
+    x_watson_learning_opt_out=True)  # Optional flag
+
 # Replace with the context obtained from the initial request
 context = {}
 
 workspace_id = '6846ffc4-07eb-45d9-a517-abe54e9f2dae'
+
+unique_filename = uuid.uuid4()
 
 response = conversation.message(
   workspace_id=workspace_id,
@@ -18,4 +29,10 @@ response = conversation.message(
   context=context
 )
 
-print(json.dumps(response, indent=2))
+answer_cont = response['output']
+answer_text = answer_cont['text']
+answer = answer_text[0]
+
+
+with open(join(dirname(__file__), 'resources/' + unique_filename.urn + '.wav'), 'wb+') as audio_file:
+    audio_file.write(text_to_speech.synthesize(answer, accept='audio/wav', voice="en-US_AllisonVoice"))
