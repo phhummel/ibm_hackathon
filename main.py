54106f8dae96019bd4b8739fedaed4477d7cb257
Jan Hendrik Kirchner <janhendrikkirchner@83add7ab.funky.uni-osnabrueck.de>
2016-12-02 14:14:50 +0100

    changed login-dates

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ import json
 from watson_developer_cloud import ConversationV1
 
 conversation = ConversationV1(
-  username='{username}',
-  password='{password}',
+  username='{d9940f19-3ca0-4154-88dd-af102b9b701f}',
+  password='{rcVDyr2CEIcg}',
   version='2016-09-20'
 )
 
 # Replace with the context obtained from the initial request
 context = {}
 
-workspace_id = '25dfa8a0-0263-471b-8980-317e68c30488'
+workspace_id = '6846ffc4-07eb-45d9-a517-abe54e9f2dae'
 
 response = conversation.message(
   workspace_id=workspace_id,
-  message_input={'text': 'Turn on the lights'},
+  message_input={'text': 'Hi'},
   context=context
 )
 
